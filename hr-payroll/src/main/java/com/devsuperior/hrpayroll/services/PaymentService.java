ba989e4f8b90be91af4cb0dff50700fbd18fd645
Rodrigo Pereira <rodrigo.pereira@sesicni.com.br>
2020-10-25 16:37:08 -0300

    restTemplate basic implementation

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -1,13 +1,25 @@
 package com.devsuperior.hrpayroll.services;
 
 import com.devsuperior.hrpayroll.entities.Payment;
+import com.devsuperior.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(Long workerId, Integer days) {
-        return new Payment("Bob", 200.00, days);
+        String url = workerHost + "/workers/" + workerId;
+        Worker worker = restTemplate.getForObject(url, Worker.class);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
 }
