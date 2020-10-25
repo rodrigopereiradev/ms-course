c922b2605d0b61affd279385fe19862fdf67cfdc
Rodrigo Pereira <rodrigo.pereira@sesicni.com.br>
2020-10-25 17:26:31 -0300

    feign client

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/feingclients/WorkerFeingClients.java
@@ -0,0 +1,17 @@
+package com.devsuperior.hrpayroll.feingclients;
+
+import com.devsuperior.hrpayroll.entities.Worker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(name = "hr-worker", url = "http://localhost:8001", path = "/workers")
+public interface WorkerFeingClients {
+
+    @GetMapping(value = "/{id}")
+    ResponseEntity<Worker> findById(@PathVariable Long id);
+
+}
--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -2,23 +2,18 @@ package com.devsuperior.hrpayroll.services;
 
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.feingclients.WorkerFeingClients;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeingClients workerFeingClients;
 
     public Payment getPayment(Long workerId, Integer days) {
-        String url = workerHost + "/workers/" + workerId;
-        Worker worker = restTemplate.getForObject(url, Worker.class);
+        Worker worker = workerFeingClients.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
