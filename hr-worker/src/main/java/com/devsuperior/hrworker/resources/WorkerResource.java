1bc8582a6b6cbd6eae013220cdcc7cf8a2a6591a
Rodrigo Pereira <rodrigo.pereira@sesicni.com.br>
2020-10-27 23:29:52 -0300

    hystrix failure tolerace

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -35,6 +35,12 @@ public class WorkerResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker > findOne(@PathVariable Long id) {
 
+//        try {
+//            Thread.sleep(3000L);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
         logger.info("PORT = " + environment.getProperty("local.server.port"));
 
         Worker worker = repository.findById(id).get();
