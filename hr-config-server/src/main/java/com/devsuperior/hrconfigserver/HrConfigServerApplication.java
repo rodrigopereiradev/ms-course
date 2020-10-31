bcf855cfdfc72deb21487799845ad640d26f7982
Rodrigo Pereira <rodrigo.pereira@sesicni.com.br>
2020-10-31 19:41:56 -0300

    config server reading properties from a private repo

--- hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
@@ -1,15 +1,26 @@
 package com.devsuperior.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
+public class HrConfigServerApplication implements CommandLineRunner {
+    @Value(value = "${spring.cloud.config.server.git.username}")
+    private String userName;
+    @Value(value = "${spring.cloud.config.server.git.password}")
+    private String password;
 
     public static void main(String[] args) {
         SpringApplication.run(HrConfigServerApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+//        System.out.println("USER NAME GIT = " + userName);
+//        System.out.println("PASSWORD = " + password);
+    }
 }
